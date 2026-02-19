8b92b067d030ee4f32410eb7ce646eec2e428b0b
Rohan-rohan-102 <rohandhakal303@gmail.com>
2026-02-19 18:34:22 +0545

    ...

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,18 @@
 import path from "path" 
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import react from "@vitejs/plugin-react" // Ensure you have the react plugin imported
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@common": path.resolve(__dirname, "./common"), // Add this line
     },
   },
 })
