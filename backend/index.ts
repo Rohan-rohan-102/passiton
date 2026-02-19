4cd1e4f85051386cc7bd2eb6b1e05fb7c14ba748
Rohan-rohan-102 <rohandhakal303@gmail.com>
2026-02-19 19:03:02 +0545

    .

--- backend/index.ts
+++ backend/index.ts
@@ -21,7 +21,7 @@ app.use(cookieParser())
 app.use(express.json())
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173', 'https://passiton-zeta.vercel.app'], 
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] // Explicitly allow DELETE 
 }));
