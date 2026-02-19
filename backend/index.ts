89ccb9456a5c1b7dc88ac2ad19891adb3d9f4de9
Rohan-rohan-102 <rohandhakal303@gmail.com>
2026-02-19 22:36:39 +0545

    ...

--- backend/index.ts
+++ backend/index.ts
@@ -37,9 +37,9 @@ app.use('/api/comment', CommentRouote)
 
 
 
-app.listen(PORT, () => {
-    console.log('Server running on port:', PORT)
-})
+app.listen(Number(PORT) || 3000, "0.0.0.0", () => {
+    console.log('Server running on port:', PORT);
+});
 
 
 app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
